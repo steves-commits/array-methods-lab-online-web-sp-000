6a0bd6d27669cdbe28484e9a1ce332c2a868bfbf
Steven Topazio <sat@northpointprinting.com>
2020-04-01 14:08:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,16 @@ array.sort
 end
 
 def using_reverse(array)
-
+array.reverse
 end
 
 def using_first(array)
-
+array.first
 end
 
 def using_last(array)
-
-end
+array.last
 
 def using_size(array)
-
+array.size
 end
